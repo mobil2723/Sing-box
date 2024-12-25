f6fc69d52d4f8d9d806e0d61f30d5ce2ca4165f7
mobil2723 <mobile2723@outlook.com>
2024-12-25 19:18:52 +0800

    Update 00.sh

--- 00.sh
+++ 00.sh
@@ -14,7 +14,7 @@ reading() { read -p "$(red "$1")" "$2"; }
 
 USERNAME=$(whoami)
 HOSTNAME=$(hostname)
-export UUID=${UUID:-'bc97f674-c578-4940-9234-0a1da46041b9'}
+export UUID=${UUID:-'0fcfe670-742d-c315-4f97-2ec237734c04'}
 export NEZHA_SERVER=${NEZHA_SERVER:-''} 
 export NEZHA_PORT=${NEZHA_PORT:-'5555'}     
 export NEZHA_KEY=${NEZHA_KEY:-''}
