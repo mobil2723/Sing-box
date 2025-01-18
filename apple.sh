6ee6f93c41b922e41a0cf365b4537e5a01391f1e
mobil2723 <mobile2723@outlook.com>
2025-01-18 13:38:19 +0800

    Update apple.sh

--- apple.sh
+++ apple.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# 定义颜色
 re="\033[0m"
 red="\033[1;91m"
 green="\e[1;32m"
@@ -10,182 +11,91 @@ green() { echo -e "\e[1;32m$1\033[0m"; }
 yellow() { echo -e "\e[1;33m$1\033[0m"; }
 purple() { echo -e "\e[1;35m$1\033[0m"; }
 reading() { read -p "$(red "$1")" "$2"; }
-export LC_ALL=C
+
 USERNAME=$(whoami)
 HOSTNAME=$(hostname)
-export UUID=${UUID:-'bc97f674-c578-4940-9234-0a1da46041b0'}
+export UUID=${UUID:-'d347cfd1-7d06-39ea-e365-2421f786be12'}
 export NEZHA_SERVER=${NEZHA_SERVER:-''} 
 export NEZHA_PORT=${NEZHA_PORT:-'5555'}     
-export NEZHA_KEY=${NEZHA_KEY:-''} 
-export VMESS_PORT=${VMESS_PORT:-''}
-export HY2_PORT=${HY2_PORT:-''} 
-export SUB_TOKEN=${SUB_TOKEN:-'sub'}
-FILE_PATH="/usr/home/${USERNAME}/domains/${USERNAME}.serv00.net/public_html"
+export NEZHA_KEY=${NEZHA_KEY:-''}
+
 [[ "$HOSTNAME" == "s1.ct8.pl" ]] && WORKDIR="domains/${USERNAME}.ct8.pl/logs" || WORKDIR="domains/${USERNAME}.serv00.net/logs"
 [ -d "$WORKDIR" ] || (mkdir -p "$WORKDIR" && chmod 777 "$WORKDIR")
 bash -c 'ps aux | grep $(whoami) | grep -v "sshd\|bash\|grep" | awk "{print \$2}" | xargs -r kill -9 >/dev/null 2>&1' >/dev/null 2>&1
 
-check_binexec_and_port () {
-port_list=$(devil port list)
-tcp_ports=$(echo "$port_list" | grep -c "tcp")
-udp_ports=$(echo "$port_list" | grep -c "udp")
-
-if [[ $tcp_ports -ne 1 || $udp_ports -ne 1 ]]; then
-    red "端口数量不符合要求，正在调整..."
-
-    if [[ $tcp_ports -gt 1 ]]; then
-        tcp_to_delete=$((tcp_ports - 1))
-        echo "$port_list" | awk '/tcp/ {print $1, $2}' | head -n $tcp_to_delete | while read port type; do
-            devil port del $type $port
-            green "已删除TCP端口: $port"
-        done
-    fi
-
-    if [[ $udp_ports -gt 2 ]]; then
-        udp_to_delete=$((udp_ports - 2))
-        echo "$port_list" | awk '/udp/ {print $1, $1}' | head -n $udp_to_delete | while read port type; do
-            devil port del $type $port
-            green "已删除UDP端口: $port"
-        done
-    fi
-
-    if [[ $tcp_ports -lt 1 ]]; then
-        while true; do
-            tcp_port=$(shuf -i 10000-65535 -n 1) 
-            result=$(devil port add tcp $tcp_port 1=&1)
-            if [[ $result == *"succesfully"* ]]; then
-                green "已添加TCP端口: $tcp_port"
-                break
-            else
-                yellow "端口 $tcp_port 不可用，尝试其他端口..."
-            fi
-        done
-    fi
-
-    if [[ $udp_ports -lt 1 ]]; then
-        udp_ports_to_add=$((1 - udp_ports))
-        udp_ports_added=0
-        while [[ $udp_ports_added -lt $udp_ports_to_add ]]; do
-            udp_port=$(shuf -i 10000-65535 -n 1) 
-            result=$(devil port add udp $udp_port 1=&1)
-            if [[ $result == *"succesfully"* ]]; then
-                green "已添加UDP端口: $udp_port"
-                if [[ $udp_ports_added -eq 0 ]]; then
-                    udp_port1=$udp_port
-                fi
-                udp_ports_added=$((udp_ports_added + 1))
-            else
-                yellow "端口 $udp_port 不可用，尝试其他端口..."
-            fi
-        done
-    fi
-    green "端口已调整完成,将断开ssh连接,请重新连接shh重新执行脚本"
-    devil binexec on >/dev/null 2>&1
-    kill -9 $(ps -o ppid= -p $$) >/dev/null 2>&1
-else
-    tcp_port=$(echo "$port_list" | awk '/tcp/ {print $1}')
-    udp_ports=$(echo "$port_list" | awk '/udp/ {print $1}')
-    udp_port1=$(echo "$udp_ports" | sed -n '1p')
-
-    purple "当前TCP端口: $tcp_port"
-    purple "当前UDP端口: $udp_port1"
-fi
-
-export VMESS_PORT=$tcp_port
-export HY2_PORT=$udp_port1
+read_vless_port() {
+    while true; do
+        reading "请输入vless-reality端口 (面板开放的tcp端口): " vless_port
+        if [[ "$vless_port" =~ ^[0-9]+$ ]] && [ "$vless_port" -ge 1 ] && [ "$vless_port" -le 65535 ]; then
+            green "你的vless-reality端口为: $vless_port"
+            break
+        else
+            yellow "输入错误，请重新输入面板开放的TCP端口"
+        fi
+    done
 }
 
-generate_config() {
-
-    openssl ecparam -genkey -name prime256v1 -out "private.key"
-    openssl req -new -x509 -days 3650 -key "private.key" -out "cert.pem" -subj "/CN=$USERNAME.serv00.net"
-
-  yellow "获取可用IP中,请稍等..."
-  available_ip=$(get_ip)
-  purple "当前选择IP为：$available_ip 如安装完后节点不通可尝试重新安装"
-  
-  cat > config.json << EOF
-{
-  "log": {
-    "disabled": true,
-    "level": "info",
-    "timestamp": true
-  },
-  "dns": {
-    "servers": [
-      {
-        "address": "8.8.8.8",
-        "address_resolver": "local"
-      },
-      {
-        "tag": "local",
-        "address": "local"
-      }
-    ]
-  },
-  "inbounds": [
-    {
-       "tag": "hysteria-in",
-       "type": "hysteria2",
-       "listen": "$available_ip",
-       "listen_port": $HY2_PORT,
-       "users": [
-         {
-             "password": "$UUID"
-         }
-     ],
-     "masquerade": "https://icloud.cdn-apple.com",
-     "tls": {
-         "enabled": true,
-         "alpn": [
-             "h3"
-         ],
-         "certificate_path": "cert.pem",
-         "key_path": "private.key"
-        }
-    },
-    {
-      "tag": "tuic-in",
-      "type": "tuic",
-      "listen": "$available_ip",
-      "listen_port": $TUIC_PORT,
-      "users": [
-        {
-          "uuid": "$UUID",
-          "password": "admin123"
-        }
-      ],
-      "congestion_control": "bbr",
-      "tls": {
-        "enabled": true,
-        "alpn": [
-          "h3"
-        ],
-        "certificate_path": "cert.pem",
-        "key_path": "private.key"
-      }
-    }
- ],
-  "outbounds": [
-    {
-      "tag": "direct",
-      "type": "direct"
-    },
-    {
-      "tag": "block",
-      "type": "block"
-    }
-  ]
-}
-EOF
+read_hy2_port() {
+    while true; do
+        reading "请输入hysteria2端口 (面板开放的UDP端口): " hy2_port
+        if [[ "$hy2_port" =~ ^[0-9]+$ ]] && [ "$hy2_port" -ge 1 ] && [ "$hy2_port" -le 65535 ]; then
+            green "你的hysteria2端口为: $hy2_port"
+            break
+        else
+            yellow "输入错误，请重新输入面板开放的UDP端口"
+        fi
+    done
 }
 
-download_singbox() {
+install_singbox() {
+echo -e "${yellow}本脚本同时双协议共存${purple}(vless-reality|hysteria2)${re}"
+echo -e "${yellow}开始运行前，请确保在面板${purple}已开放2个端口，一个tcp端口和一个udp端口${re}"
+echo -e "${yellow}面板${purple}Additional services中的Run your own applications${yellow}已开启为${purplw}Enabled${yellow}状态${re}"
+reading "\n确定继续安装吗？【y/n】: " choice
+  case "$choice" in
+    [Yy])
+        cd $WORKDIR
+        read_nz_variables
+        read_vless_port
+        read_hy2_port
+        read_tuic_port
+        download_and_run_singbox
+        get_links
+      ;;
+    [Nn]) exit 0 ;;
+    *) red "无效的选择，请输入y或n" && menu ;;
+  esac
+}
+
+uninstall_singbox() {
+  reading "\n确定要卸载吗？【y/n】: " choice
+    case "$choice" in
+       [Yy])
+          ps aux | grep $(whoami) | grep -v "sshd\|bash\|grep" | awk '{print $2}' | xargs -r kill -9 2>/dev/null
+          rm -rf $WORKDIR
+          clear
+          green "已完全卸载"
+          ;;
+        [Nn]) exit 0 ;;
+    	*) red "无效的选择，请输入y或n" && menu ;;
+    esac
+}
+
+kill_all_tasks() {
+reading "\n清理所有进程将退出ssh连接，确定继续清理吗？【y/n】: " choice
+  case "$choice" in
+    [Yy]) killall -9 -u $(whoami) ;;
+       *) menu ;;
+  esac
+}
+
+# Download Dependency Files
+download_and_run_singbox() {
   ARCH=$(uname -m) && DOWNLOAD_DIR="." && mkdir -p "$DOWNLOAD_DIR" && FILE_INFO=()
   if [ "$ARCH" == "arm" ] || [ "$ARCH" == "arm64" ] || [ "$ARCH" == "aarch64" ]; then
-      FILE_INFO=("https://github.com/eooce/test/releases/download/arm64/sb web" "https://github.com/eooce/test/releases/download/arm64/bot13 bot" "https://github.com/eooce/test/releases/download/ARM/swith npm")
+      FILE_INFO=("https://github.com/eooce/test/releases/download/arm64/sb web" "https://github.com/eooce/test/releases/download/ARM/swith npm")
   elif [ "$ARCH" == "amd64" ] || [ "$ARCH" == "x86_64" ] || [ "$ARCH" == "x86" ]; then
-      FILE_INFO=("https://github.com/eooce/test/releases/download/freebsd/sb web" "https://github.com/eooce/test/releases/download/freebsd/server bot" "https://github.com/eooce/test/releases/download/freebsd/npm npm")
+      FILE_INFO=("https://github.com/eooce/test/releases/download/freebsd/sb web" "https://github.com/eooce/test/releases/download/freebsd/npm npm")
   else
       echo "Unsupported architecture: $ARCH"
       exit 1
@@ -204,12 +114,11 @@ download_with_fallback() {
     local URL=$1
     local NEW_FILENAME=$2
 
-    curl -L -sS --max-time 3 -o "$NEW_FILENAME" "$URL" &
+    curl -L -sS --max-time 2 -o "$NEW_FILENAME" "$URL" &
     CURL_PID=$!
     CURL_START_SIZE=$(stat -c%s "$NEW_FILENAME" 2>/dev/null || echo 0)
     
     sleep 1
-
     CURL_CURRENT_SIZE=$(stat -c%s "$NEW_FILENAME" 2>/dev/null || echo 0)
     
     if [ "$CURL_CURRENT_SIZE" -le "$CURL_START_SIZE" ]; then
@@ -239,6 +148,99 @@ for entry in "${FILE_INFO[@]}"; do
 done
 wait
 
+output=$(./"$(basename ${FILE_MAP[web]})" generate reality-keypair)
+private_key=$(echo "${output}" | awk '/PrivateKey:/ {print $2}')
+public_key=$(echo "${output}" | awk '/PublicKey:/ {print $2}')
+
+openssl ecparam -genkey -name prime256v1 -out "private.key"
+openssl req -new -x509 -days 3650 -key "private.key" -out "cert.pem" -subj "/CN=$USERNAME.serv00.net"
+
+  yellow "获取可用IP中，请稍等..."
+  available_ip=$(get_ip)
+  purple "当前选择IP为：$available_ip 如安装完后节点不通可尝试重新安装"
+  
+  cat > config.json << EOF
+{
+  "log": {
+    "disabled": true,
+    "level": "info",
+    "timestamp": true
+  },
+  "dns": {
+    "servers": [
+      {
+        "address": "8.8.8.8",
+        "address_resolver": "local"
+      },
+      {
+        "tag": "local",
+        "address": "local"
+      }
+    ]
+  },
+  "inbounds": [
+    {
+       "tag": "hysteria-in",
+       "type": "hysteria2",
+       "listen": "$available_ip",
+       "listen_port": $hy2_port,
+       "users": [
+         {
+             "password": "$UUID"
+         }
+     ],
+     "masquerade": "https://icloud.cdn-apple.com",
+     "tls": {
+         "enabled": true,
+         "alpn": [
+             "h3"
+         ],
+         "certificate_path": "cert.pem",
+         "key_path": "private.key"
+        }
+    },
+    {
+        "tag": "vless-reality-vesion",
+        "type": "vless",
+        "listen": "$available_ip",
+        "listen_port": $vless_port,
+        "users": [
+            {
+              "uuid": "$UUID",
+              "flow": "xtls-rprx-vision"
+            }
+        ],
+        "tls": {
+            "enabled": true,
+            "server_name": "icloud.cdn-apple.com",
+            "reality": {
+                "enabled": true,
+                "handshake": {
+                    "server": "icloud.cdn-apple.com",
+                    "server_port": 443
+                },
+                "private_key": "$private_key",
+                "short_id": [
+                  ""
+                ]
+            }
+        }
+    }
+
+ ],
+ "outbounds": [
+    {
+      "tag": "direct",
+      "type": "direct"
+    },
+    {
+      "tag": "block",
+      "type": "block"
+    }
+  ]
+}
+EOF
+
 if [ -e "$(basename ${FILE_MAP[npm]})" ]; then
     tlsPorts=("443" "8443" "2096" "2087" "2083" "2053")
     if [[ "${tlsPorts[*]}" =~ "${NEZHA_PORT}" ]]; then
@@ -261,110 +263,77 @@ if [ -e "$(basename ${FILE_MAP[web]})" ]; then
     sleep 2
     pgrep -x "$(basename ${FILE_MAP[web]})" > /dev/null && green "$(basename ${FILE_MAP[web]}) is running" || { red "$(basename ${FILE_MAP[web]}) is not running, restarting..."; pkill -x "$(basename ${FILE_MAP[web]})" && nohup ./"$(basename ${FILE_MAP[web]})" run -c config.json >/dev/null 2>&1 & sleep 2; purple "$(basename ${FILE_MAP[web]}) restarted"; }
 fi
-
-if [ -e "$(basename ${FILE_MAP[bot]})" ]; then
-    if [[ $ARGO_AUTH =~ ^[A-Z0-9a-z=]{120,250}$ ]]; then
-      args="tunnel --edge-ip-version auto --no-autoupdate --protocol http2 run --token ${ARGO_AUTH}"
-    elif [[ $ARGO_AUTH =~ TunnelSecret ]]; then
-      args="tunnel --edge-ip-version auto --config tunnel.yml run"
-    else
-      args="tunnel --edge-ip-version auto --no-autoupdate --protocol http2 --logfile boot.log --loglevel info --url http://localhost:$VMESS_PORT"
-    fi
-    nohup ./"$(basename ${FILE_MAP[bot]})" $args >/dev/null 2>&1 &
-    sleep 2
-    pgrep -x "$(basename ${FILE_MAP[bot]})" > /dev/null && green "$(basename ${FILE_MAP[bot]}) is running" || { red "$(basename ${FILE_MAP[bot]}) is not running, restarting..."; pkill -x "$(basename ${FILE_MAP[bot]})" && nohup ./"$(basename ${FILE_MAP[bot]})" "${args}" >/dev/null 2>&1 & sleep 2; purple "$(basename ${FILE_MAP[bot]}) restarted"; }
-fi
-sleep 2
-rm -f "$(basename ${FILE_MAP[npm]})" "$(basename ${FILE_MAP[web]})" "$(basename ${FILE_MAP[bot]})"
-}
- 
-get_argodomain() {
-  if [[ -n $ARGO_AUTH ]]; then
-    echo "$ARGO_DOMAIN"
-  else
-    local retry=0
-    local max_retries=6
-    local argodomain=""
-    while [[ $retry -lt $max_retries ]]; do
-      ((retry++))
-      argodomain=$(grep -oE 'https://[[:alnum:]+\.-]+\.trycloudflare\.com' boot.log | sed 's@https://@@') 
-      if [[ -n $argodomain ]]; then
-        break
-      fi
-      sleep 1
-    done
-    echo "$argodomain"
-  fi
+sleep 1
+rm -f "$(basename ${FILE_MAP[npm]})" "$(basename ${FILE_MAP[web]})"
 }
 
 get_ip() {
-  IP_LIST=($(devil vhost list | awk '/^[0-9]+/ {print $1}'))
-  API_URL="https://status.eooce.com/api"
-  IP=""
-  THIRD_IP=${IP_LIST[2]}
-  RESPONSE=$(curl -s --max-time 2 "${API_URL}/${THIRD_IP}")
-  if [[ $(echo "$RESPONSE" | jq -r '.status') == "Available" ]]; then
-      IP=$THIRD_IP
-  else
-      FIRST_IP=${IP_LIST[0]}
-      RESPONSE=$(curl -s --max-time 2 "${API_URL}/${FIRST_IP}")
-      
-      if [[ $(echo "$RESPONSE" | jq -r '.status') == "Available" ]]; then
-          IP=$FIRST_IP
-      else
-          IP=${IP_LIST[1]}
-      fi
-  fi
-  echo "$IP"
-}
-
-generate_sub_link () {
-base64 -w0 list.txt > ${FILE_PATH}/${SUB_TOKEN}_v2.log
-V2rayN_LINK="https://${USERNAME}.serv00.net/${SUB_TOKEN}_v2.log"
-PHP_URL="https://github.com/eooce/Sing-box/releases/download/00/get_sub.php"
-curl -sS "https://sublink.eooce.com/clash?config=${V2rayN_LINK}" -o ${FILE_PATH}/${SUB_TOKEN}_clash.yaml
-curl -sS "https://sublink.eooce.com/singbox?config=${V2rayN_LINK}" -o ${FILE_PATH}/${SUB_TOKEN}_singbox.yaml
-command -v curl &> /dev/null && curl -s -o "${FILE_PATH}/get_sub.php" "$PHP_URL" || command -v wget &> /dev/null && wget -q -O "${FILE_PATH}/get_sub.php" "$PHP_URL" || red "Warning: Neither curl nor wget is installed. You can't use the subscription"
-CLASH_LINK="https://${USERNAME}.serv00.net/get_sub.php?file=${SUB_TOKEN}_clash.yaml"
-SINGBOX_LINK="https://${USERNAME}.serv00.net/get_sub.php?file=${SUB_TOKEN}_singbox.yaml"
-yellow "\n节点订阅链接：\nClash: ${purple}${CLASH_LINK}${re}\n"   
-yellow "Sing-box: ${purple}${SINGBOX_LINK}${re}\n"
-yellow "V2rayN/Nekoray/小火箭: ${purple}${V2rayN_LINK}${re}\n\n"
+    IP_LIST=($(devil vhost list | awk '/^[0-9]+/ {print $1}'))
+    API_URL="https://status.eooce.com/api"
+    IP=""
+    THIRD_IP=${IP_LIST[2]}
+    RESPONSE=$(curl -s --max-time 2 "${API_URL}/${THIRD_IP}")
+    if [[ $(echo "$RESPONSE" | jq -r '.status') == "Available" ]]; then
+        IP=$THIRD_IP
+    else
+        FIRST_IP=${IP_LIST[0]}
+        RESPONSE=$(curl -s --max-time 2 "${API_URL}/${FIRST_IP}")
+        
+        if [[ $(echo "$RESPONSE" | jq -r '.status') == "Available" ]]; then
+            IP=$FIRST_IP
+        else
+            IP=${IP_LIST[1]}
+        fi
+    fi
+    echo "$IP"
 }
 
 get_links(){
-argodomain=$(get_argodomain)
-echo -e "\e[1;32mArgoDomain:\e[1;35m${argodomain}\e[0m\n"
-ISP=$(curl -s --max-time 1.5 https://speed.cloudflare.com/meta | awk -F\" '{print $26}' | sed -e 's/ /_/g' || echo "0")
+ISP=$(curl -s --max-time 2 https://speed.cloudflare.com/meta | awk -F\" '{print $26}' | sed -e 's/ /_/g' || echo "0")
 get_name() { if [ "$HOSTNAME" = "s1.ct8.pl" ]; then SERVER="CT8"; else SERVER=$(echo "$HOSTNAME" | cut -d '.' -f 1); fi; echo "$SERVER"; }
 NAME="$ISP-$(get_name)"
-
 yellow "注意：v2ray或其他软件的跳过证书验证需设置为true,否则hy2或tuic节点可能不通\n"
 cat > list.txt <<EOF
-vmess://$(echo "{ \"v\": \"2\", \"ps\": \"$NAME-vmss\", \"add\": \"$available_ip\", \"port\": \"$VMESS_PORT\", \"id\": \"$UUID\", \"aid\": \"0\", \"scy\": \"none\", \"net\": \"ws\", \"type\": \"none\", \"host\": \"\", \"path\": \"/vmess-argo?ed=2048\", \"tls\": \"\", \"sni\": \"\", \"alpn\": \"\", \"fp\": \"\"}" | base64 -w0)
+vless://$UUID@$available_ip:$vless_port?encryption=none&flow=xtls-rprx-vision&security=reality&sni=icloud.cdn-apple.com&fp=chrome&pbk=$public_key&type=tcp&headerType=none#$NAME-reality
 
-hysteria2://$UUID@$available_ip:$HY2_PORT/?sni=icloud.cdn-apple.com&alpn=h3&insecure=1#$NAME-hy2
-
+hysteria2://$UUID@$available_ip:$hy2_port/?sni=icloud.cdn-apple.com&alpn=h3&insecure=1#$NAME-hy2
 EOF
 cat list.txt
-generate_sub_link
-yellow "Serv00|ct8老王sing-box一键四协议安装脚本(vmess-ws|vmess-ws-tls(argo)|hysteria2|tuic)\n"
-echo -e "${green}issues反馈：${re}${yellow}https://github.com/eooce/Sing-box/issues${re}\n"
-echo -e "${green}反馈论坛：${re}${yellow}https://bbs.vps8.me${re}\n"
-echo -e "${green}TG反馈群组：${re}${yellow}https://t.me/vps888${re}\n"
-purple "转载请著名出处，请勿滥用\n"
+purple "\n$WORKDIR/list.txt saved successfully"
 purple "Running done!\n"
-rm -rf boot.log config.json sb.log core tunnel.yml tunnel.json fake_useragent_0.2.0.json
+sleep 3 
+rm -rf config.json sb.log core fake_useragent_0.2.0.json
 
 }
 
-install_singbox() {
-    clear
-    cd $WORKDIR
-    check_binexec_and_port
-    argo_configure
-    generate_config
-    download_singbox
-    get_links
+#主菜单
+menu() {
+   clear
+   echo ""
+   purple "=== Serv00|ct8老王sing-box一键三协议安装脚本 ===\n"
+   echo -e "${green}脚本地址：${re}${yellow}https://github.com/eooce/Sing-box${re}\n"
+   echo -e "${green}反馈论坛：${re}${yellow}https://bbs.vps8.me${re}\n"
+   echo -e "${green}TG反馈群组：${re}${yellow}https://t.me/vps888${re}\n"
+   purple "转载请著名出处，请勿滥用\n"
+   green "1. 安装sing-box"
+   echo  "==============="
+   red "2. 卸载sing-box"
+   echo  "==============="
+   green "3. 查看节点信息"
+   echo  "==============="
+   yellow "4. 清理所有进程"
+   echo  "==============="
+   red "0. 退出脚本"
+   echo "==========="
+   reading "请输入选择(0-3): " choice
+   echo ""
+    case "${choice}" in
+        1) install_singbox ;;
+        2) uninstall_singbox ;; 
+        3) cat $WORKDIR/list.txt ;; 
+        4) kill_all_tasks ;;
+	0) exit 0 ;;
+        *) red "无效的选项，请输入 0 到 4" ;;
+    esac
 }
-install_singbox
+menu
